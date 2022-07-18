6f775770e8a3e0fb70d0cb42bdb6ae875d42564e
Awfulot <Mr.Snisken1@gmail.com>
2022-07-18 15:57:54 -0400

    Removed internal JS and added it to external script

--- jscalc.js
+++ jscalc.js
@@ -1,5 +1,26 @@
-var result = 0;
-var numbers = [];
-var operators = [];
 
+    //function that display value
+    function dis(val)
+    {
+    document.getElementById("result").value+=val;
+    }
+  
+    function clearScreen()
+    {
+        document.getElementById("result").value="";
+    }
+  
+ 
+    function calc()
+    {
+      
+      let x=document.getElementById("result").value
+      let y=eval(x)
+      
+      document.getElementById("result").value=y
+
+
+    }
+
+  
 
